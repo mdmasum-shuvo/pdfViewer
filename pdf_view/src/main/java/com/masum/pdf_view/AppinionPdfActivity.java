5aeb7e1781b38130c75b9d7b834f5722365b10d6
masum <mdmasum.shuvo.cse@gmail.com>
2021-02-16 14:48:59 +0600

    commit

--- pdf_view/src/main/java/com/masum/pdf_view/AppinionPdfActivity.java
+++ pdf_view/src/main/java/com/masum/pdf_view/AppinionPdfActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.mypdfviewlibrary.Constant;
 
@@ -16,6 +18,11 @@ public class AppinionPdfActivity extends AppCompatActivity {
     private WebView pdfView;
     private ProgressBar progress;
     private String removePdfTopIcon = "javascript:(function() {" + "document.querySelector('[role=\"toolbar\"]').remove();})()";
+    private int i = 0;
+    private ImageView imageViewLeft, imageViewRight;
+    private String[] strArray = {
+            "http://www.pdf995.com/samples/pdf.pdf"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +32,34 @@ public class AppinionPdfActivity extends AppCompatActivity {
         pdfView = findViewById(R.id.pdfView);
         progress = findViewById(R.id.progress);
         String url=getIntent().getStringExtra("pdf");
+        imageViewLeft = findViewById(R.id.iv_previous);
+        imageViewRight = findViewById(R.id.iv_next);
         showPdfFile(url);
 
+
+
+        imageViewLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (i != 0) {
+                    showPdfFile(strArray[i--]);
+                } else {
+                    Toast.makeText(getApplicationContext(), "You're on first PDF", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        imageViewRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (i != strArray.length) {
+                    showPdfFile(strArray[i++]);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Going to first PDF", Toast.LENGTH_SHORT).show();
+                    showPdfFile(strArray[0]);
+                }
+            }
+        });
 
     }
 
